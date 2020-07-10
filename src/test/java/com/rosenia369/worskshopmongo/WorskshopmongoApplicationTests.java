bd3baeafc1dbd7c8e88ab575998f7900f6eb1a16
Rosenia Sa <sandrarosenia@gmail.com>
2020-07-10 19:00:10 +0100

    Primeiro commit projeto criado

---
+++ src/test/java/com/rosenia369/worskshopmongo/WorskshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rosenia369.worskshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorskshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
