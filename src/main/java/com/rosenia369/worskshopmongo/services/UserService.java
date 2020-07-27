a572d7ab894125e55edf85de717f3e164275a6a7
Rosenia Sa <sandrarosenia@gmail.com>
2020-07-27 11:27:06 +0100

    Insercao de usuario com POST

--- src/main/java/com/rosenia369/worskshopmongo/services/UserService.java
+++ src/main/java/com/rosenia369/worskshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rosenia369.worskshopmongo.domain.User;
+import com.rosenia369.worskshopmongo.dto.UserDTO;
 import com.rosenia369.worskshopmongo.repository.UserRepository;
 import com.rosenia369.worskshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
