a211488c18533e8a87147bf87e5c4d90e0568fae
Rosenia Sa <sandrarosenia@gmail.com>
2020-07-27 18:27:34 +0100

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/rosenia369/worskshopmongo/resources/UserResource.java
+++ src/main/java/com/rosenia369/worskshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.rosenia369.worskshopmongo.domain.Post;
 import com.rosenia369.worskshopmongo.domain.User;
 import com.rosenia369.worskshopmongo.dto.UserDTO;
 import com.rosenia369.worskshopmongo.services.UserService;
@@ -58,5 +59,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
