756df7ace45821455c19068ba8bb5c849ab11d8b
Rosenia Sa <sandrarosenia@gmail.com>
2020-07-27 10:13:31 +0100

    Obtendo um usuario por id

--- src/main/java/com/rosenia369/worskshopmongo/services/UserService.java
+++ src/main/java/com/rosenia369/worskshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.rosenia369.worskshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rosenia369.worskshopmongo.domain.User;
 import com.rosenia369.worskshopmongo.repository.UserRepository;
+import com.rosenia369.worskshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
