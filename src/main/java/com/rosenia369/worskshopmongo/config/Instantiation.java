911ac3c596918ba13f4288923862fe497340809d
Rosenia Sa <sandrarosenia@gmail.com>
2020-07-28 09:35:12 +0100

    Acrescentando comentaios aos posts

--- src/main/java/com/rosenia369/worskshopmongo/config/Instantiation.java
+++ src/main/java/com/rosenia369/worskshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.rosenia369.worskshopmongo.domain.Post;
 import com.rosenia369.worskshopmongo.domain.User;
 import com.rosenia369.worskshopmongo.dto.AuthorDTO;
+import com.rosenia369.worskshopmongo.dto.CommentDTO;
 import com.rosenia369.worskshopmongo.repository.PostRepository;
 import com.rosenia369.worskshopmongo.repository.UserRepository;
 
@@ -40,6 +41,13 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!",new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!",sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Manda ver cara",sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Blza, otimo dia",sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
