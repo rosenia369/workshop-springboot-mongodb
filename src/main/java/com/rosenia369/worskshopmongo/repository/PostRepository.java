d4fc25f1c853bdb702fe09dea7e00f49f08821e3
Rosenia Sa <sandrarosenia@gmail.com>
2020-07-28 10:18:11 +0100

    Consulta simples com query methods

--- src/main/java/com/rosenia369/worskshopmongo/repository/PostRepository.java
+++ src/main/java/com/rosenia369/worskshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.rosenia369.worskshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.rosenia369.worskshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post,String>{
 
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
